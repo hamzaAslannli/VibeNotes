b1d7fa52c2d2f691963e99316b0f331a94c24800
agile-bytee <agilebyte1411@gmail.com>
2026-02-12 18:48:09 +0300

    clarification

--- lib/core/theme/app_theme.dart
+++ lib/core/theme/app_theme.dart
@@ -33,7 +33,7 @@ class AppTheme {
       foregroundColor: Colors.white,
       elevation: 8,
     ),
-    cardTheme: CardTheme(
+    cardTheme: CardThemeData(
       color: const Color(0xFF1E1E1E),
       elevation: 4,
       shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(16)),
